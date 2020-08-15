81dca0feb900b002bd75cb6582edceb5cd786666
Kazuki Taniguchi <kzk018@gmail.com>
2020-08-15 15:41:25 +0900

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Command Line Tools for Xcode
+xcode-select --install
+
+# Homebrew
+if [ ! -x "`which brew`" ]; then
+  echo "Install homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+  brew update
+  brew upgrade --all --cleanup
+fi
+
+# Git
+echo "Install git..."
+brew install git
+
